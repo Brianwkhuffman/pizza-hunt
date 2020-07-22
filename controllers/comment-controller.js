8e559de0280597b6dd73308c8764cac964133829
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-22 13:12:12 -1000

    adds comment replies functionality full-stack

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -25,6 +25,25 @@ const commentController = {
       });
   },
 
+  // add reply
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza is found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        return res.json(err);
+      });
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -51,6 +70,19 @@ const commentController = {
       .catch((err) => {
         res.json(err);
       });
+  },
+
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
   },
 };
 
